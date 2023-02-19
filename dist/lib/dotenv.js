c615a715a9cd2541b22ce9ac9e3f6ebfebbf81d8
Charlotte Neau <charlotte.neau.cn@gmail.com>
2023-02-19 17:55:09 +0100

    trying to fix TSC

--- dist/lib/dotenv.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FASTIFY_PORT = exports.FASTIFY_ADDR = void 0;
-exports.FASTIFY_ADDR = "127.0.0.1";
-exports.FASTIFY_PORT = 8080;
-//# sourceMappingURL=dotenv.js.map
--- dist/specs/fixtures/session-fixture.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sessionFixture = void 0;
-const sessionFixture = function () {
-    const session = buildSessionFixture();
-};
-exports.sessionFixture = sessionFixture;
-//# sourceMappingURL=session-fixture.js.map
